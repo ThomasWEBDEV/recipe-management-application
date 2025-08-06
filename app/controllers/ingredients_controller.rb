7cf5717488e4530677b9b1f8db5716480545960b
Feret Thomas <thomas.feret@hotmail.fr>
2025-08-06 17:19:11 +0200

    implement all CRUD action for recipes and ingredients controllers

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,10 +1,31 @@
 class IngredientsController < ApplicationController
+  before_action :authenticate_user!
+
   def new
+    @recipe = Recipe.find(params[:recipe_id])
+    @ingredient = @recipe.ingredients.new
   end
 
   def create
+    @recipe = Recipe.find(params[:recipe_id])
+    @ingredient = @recipe.ingredients.new(ingredient_params)
+    if @ingredient.save
+      redirect_to @recipe
+    else
+      render :new
+    end
   end
 
   def destroy
+    @recipe = Recipe.find(params[:recipe_id])
+    @ingredient = @recipe.ingredients.find(params[:id])
+    @ingredient.destroy
+    redirect_to @recipe
+  end
+
+  private
+
+  def ingredient_params
+    params.require(:ingredient).permit(:name, :quantity)
   end
 end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,22 +1,49 @@
 class RecipesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
+    @recipes = Recipe.all
   end
 
   def show
+    @recipe = Recipe.find(params[:id])
   end
 
   def new
+    @recipe = current_user.recipes.new
   end
 
   def create
+    @recipe = current_user.recipes.new(recipe_params)
+    if @recipe.save
+      redirect_to @recipe
+    else
+      render :new
+    end
   end
 
   def edit
+    @recipe = current_user.recipes.find(params[:id])
   end
 
   def update
+    @recipe = current_user.recipes.find(params[:id])
+    if @recipe.update(recipe_params)
+      redirect_to @recipe
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @recipe = current_user.recipes.find(params[:id])
+    @recipe.destroy
+    redirect_to recipes_path
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :description, :category)
   end
 end
