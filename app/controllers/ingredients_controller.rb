190c6c11bb4cb8ccb0406c89a7200b190fd4054d
Feret Thomas <thomas.feret@hotmail.fr>
2025-08-06 17:14:52 +0200

    generate recipes and ingredients controllers

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,10 @@
+class IngredientsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'ingredients/new'
+  get 'ingredients/create'
+  get 'ingredients/destroy'
+  get 'recipes/index'
+  get 'recipes/show'
+  get 'recipes/new'
+  get 'recipes/create'
+  get 'recipes/edit'
+  get 'recipes/update'
+  get 'recipes/destroy'
   devise_for :users
   root to: "recipes#index"
 
