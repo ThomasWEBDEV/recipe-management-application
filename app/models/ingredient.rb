4562706231d5230f1ab7c26997c5b0d4d22e4635
Feret Thomas <thomas.feret@hotmail.fr>
2025-08-06 17:08:46 +0200

    Implement models relationships and validations

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,6 @@
 class Ingredient < ApplicationRecord
   belongs_to :recipe
+
+  validates :name, presence: true, length: { minimum: 2, maximum: 100 }
+  validates :quantity, presence: true, length: { maximum: 50 }
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,8 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+  has_many :ingredients, dependent: :destroy
+
+  validates :title, presence: true, length: { minimum: 3, maximum: 100 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 1000 }
+  validates :category, presence: true, inclusion: { in: %w[entrée plat dessert] }
 end
