0c97697e8632c15de9383bbb769887acfc06bf4a
Feret Thomas <thomas.feret@hotmail.fr>
2025-08-14 14:09:10 +0200

    Add search method to Recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,4 +5,14 @@ class Recipe < ApplicationRecord
   validates :title, presence: true, length: { minimum: 3, maximum: 100 }
   validates :description, presence: true, length: { minimum: 10, maximum: 1000 }
   validates :category, presence: true, inclusion: { in: %w[entrée plat dessert] }
+
+  # Méthode de recherche
+  def self.search(query)
+    if query.present?
+      where("title ILIKE :query OR description ILIKE :query OR category ILIKE :query",
+            query: "%#{query}%")
+    else
+      all
+    end
+  end
 end
