ade1a630a8d6bafaea7999c8286179aeee862e89
Feret Thomas <thomas.feret@hotmail.fr>
2025-08-26 12:51:08 +0200

    Add difficulty field to recipes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_08_21_152229) do
+ActiveRecord::Schema[7.1].define(version: 2025_08_26_105106) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,6 +33,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_21_152229) do
     t.datetime "updated_at", null: false
     t.integer "prep_time"
     t.integer "cook_time"
+    t.string "difficulty"
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
